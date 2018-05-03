25b2cc8d24e799bd8a01e358f76bc86049805c53
Matt <mr123789@gmail.com>
2018-05-04 00:52:03 +0100

    Commit 6 - Finished all portfolio pages for now. Homepage redesigned, gallery partly done.

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,22 +23,22 @@ $(document).ready(function(){
       $(".card").removeClass("active");
       $("#" + id).addClass("active");
 
-      let previous = "#" + $(".dot-active").attr("id");
-      $(previous).removeClass("dot-active");
-      $(previous).addClass("dot");
+      let previous = document.documentElement.getElementsByClassName("nav-current");
+      for(let i = 0; i < previous.length; i++){
+        previous[i].classList.remove("nav-current");
+      }
+    
+      let navBtns = document.documentElement.getElementsByClassName("nav-btn");
 
       switch (id){
-          case "home":
-              $("#dot-1").addClass("dot-active");
-              $("#dot-1").removeClass("dot");
-              break;
-          case "about":
-              $("#dot-2").addClass("dot-active");
-              $("#dot-2").removeClass("dot");
-              break;
-          case "gallery":
-              $("#dot-3").addClass("dot-active");
-              $("#dot-3").removeClass("dot");
+        case "home":
+          navBtns[0].classList.add("nav-current");
+          break;
+        case "about":
+          navBtns[1].classList.add("nav-current");
+          break;
+        case "gallery":
+          navBtns[2].classList.add("nav-current");
       }  
     })
   });
@@ -74,3 +74,25 @@ function gotoGallery() {
   }, 500);
 
 }
+
+
+document.addEventListener("scroll", function(){
+
+  if(document.documentElement.scrollTop > 2 && document.getElementById("logo").style.marginTop == "2em"){
+    $("#logo").stop(true).animate({
+      //"width": "5%",
+      //"margin-left": "46.5%",
+      "margin-top": ".8em",
+      "margin-bottom": "-2.5em"
+    }, 150);
+    //document.getElementById("header").style.boxShadow = "0 5px 15px #aaaaaa";
+  }else if(document.documentElement.scrollTop <= 2){
+    $("#logo").stop(true).animate({
+      //"width": "8%",
+      //"margin-left": "45%",
+      "margin-top": "2em",
+      "margin-bottom": ".5em"
+    }, 150);
+    //document.getElementById("header").style.boxShadow = "none";
+  }
+});
